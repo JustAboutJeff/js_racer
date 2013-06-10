e8a10d9b312434a6791efca0aa4e276ccb19c048
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-10 16:42:34 -0500

    Adds game over logic

--- script.js
+++ script.js
@@ -1,37 +1,42 @@
-$(document).ready(function() {
+$(document).ready(acceptKeyToMove);
 
-  for(var i = 0; i < 36; i++) {
-    $('tr#player1_strip').append('<td></td>');
-    $('tr#player2_strip').append('<td></td>');
-    $('tr#player3_strip').append('<td></td>');
-    $('tr#player4_strip').append('<td></td>');
-  };
+function gameOver() {
+  return $('tr.strip td:last-child').hasClass('active');
+};
 
-  $(document).keyup(function(event) {
-    var key = event.which;
-    acceptKeyToMove(key);
-  });
-
-});
-
-
-acceptKeyToMove = function(key) {
-  switch(key)
-    {
-    case 192: //RED BIKE
-      $('#player1_strip > td.active').removeClass('active').next().addClass('active');
-      break;
-    case 90: //BLUE BIKE
-      $('#player2_strip > td.active').removeClass('active').next().addClass('active');
-      break;
-    case 220: //GREEN BIKE
-      $('#player3_strip > td.active').removeClass('active').next().addClass('active');
-      break;
-    case 191: //PINK BIKE
-      $('#player4_strip > td.active').removeClass('active').next().addClass('active');
-      break;
-    default:
-      break;
+function buildTrack(size) {
+  for(var i = 0; i < size; i++) {
+    $('tr.strip').append('<td></td>');
   }
+};
+
+function acceptKeyToMove() {
+  buildTrack(36);
+  $(document).keyup(function(event) {
+    if (gameOver()) {
+      $(document).unbind('keyup');
+      alert("GAME OVER!");
+    }
+    else {
+      event.preventDefault();
+      var key = event.which;
+        switch(key) {
+          case 192: //RED BIKE
+            $('#player1_strip > td.active').removeClass('active').next().addClass('active');
+            break;
+          case 90: //BLUE BIKE
+            $('#player2_strip > td.active').removeClass('active').next().addClass('active');
+            break;
+          case 220: //GREEN BIKE
+            $('#player3_strip > td.active').removeClass('active').next().addClass('active');
+            break;
+          case 191: //PINK BIKE
+            $('#player4_strip > td.active').removeClass('active').next().addClass('active');
+            break;
+          default:
+            break;
+      }
+    }
+  });
 };
 
